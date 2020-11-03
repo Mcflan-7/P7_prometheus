0d645229f411cfce1848ff17e8a8e128efa5198c
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2020-11-03 12:29:06 +0100

    Responsive, js

--- prometheus/static/js/scripts.js
+++ prometheus/static/js/scripts.js
@@ -1,5 +1,44 @@
+/* Main JS for P7 - https://github.com/Mcflan-7/P7_prometheus  */
 'use strict'
 
-document.querySelector(".askinput").insertAdjacentText('afterbegin', "kkk")
+// MODAL SETTINGS 
 
-document.body.style.backgroundColor = "red";
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// Hamburger menu
+
+function displayMenu() {
+  const burgerMenu = document.getElementById("myLinks");
+  if (burgerMenu.style.display === "block") {
+    burgerMenu.style.display = "none";
+  } else {
+    burgerMenu.style.display = "block";
+  }
+}
